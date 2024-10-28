28aaf68fa6780030f6e59bc71974e3dda220013c
bbobsang <bbobsang@naver.com>
2024-10-28 13:41:30 +0900

    add: EmailException 생성

---
+++ src/main/java/com/sparta/spartascheduler/exception/InvalidEmailException.java
@@ -0,0 +1,7 @@
+package com.sparta.spartascheduler.exception;
+
+public class InvalidEmailException extends Exception {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
