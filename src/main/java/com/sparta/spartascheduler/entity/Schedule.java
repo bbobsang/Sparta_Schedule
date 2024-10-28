37c1a3e29b93ffbd3687bccf177c6c64ff6e4dfd
bbobsang <bbobsang@naver.com>
2024-10-28 16:33:04 +0900

    feat: 날짜, 위치, 설명 필드 추가

--- src/main/java/com/sparta/spartascheduler/entity/Schedule.java
+++ src/main/java/com/sparta/spartascheduler/entity/Schedule.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,11 +18,17 @@ import java.util.List;
 public class Schedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
+    private Long id; // 스케쥴 ID
+
     private String userName;
     private String userId; // 작성 유저명
     private String title; // 할일 제목
     private String content; // 할일 내용
+    private String weather; // 날씨 정보를 저장할 필드 추가
+    private String location; // 위치 정보
+    private LocalDate date; // 날짜 (LocalDate 타입으로 설정)
+
     @CreatedDate
     private LocalDateTime createdDate; // 작성일
     @LastModifiedDate
