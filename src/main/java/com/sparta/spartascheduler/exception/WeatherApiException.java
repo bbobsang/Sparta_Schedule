99c447ab08ae7df5218bff16a4a1890b4c562a5c
bbobsang <bbobsang@naver.com>
2024-10-28 16:33:51 +0900

    feat: WeatherApiException 클래스 추가

---
+++ src/main/java/com/sparta/spartascheduler/exception/WeatherApiException.java
@@ -0,0 +1,7 @@
+package com.sparta.spartascheduler.exception;
+
+public class WeatherApiException extends RuntimeException {
+    public WeatherApiException(String message) {
+        super(message);
+    }
+}
