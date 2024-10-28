cd32525779c99ebeb8269ea4069de755f623a0f2
bbobsang <bbobsang@naver.com>
2024-10-28 13:37:10 +0900

    refactor: validate() 메서드 사용

--- src/main/java/com/sparta/spartascheduler/service/UserService.java
+++ src/main/java/com/sparta/spartascheduler/service/UserService.java
@@ -1,6 +1,8 @@
 package com.sparta.spartascheduler.service;
 
 import com.sparta.spartascheduler.entity.User;
+import com.sparta.spartascheduler.exception.InvalidEmailException;
+import com.sparta.spartascheduler.exception.InvalidPasswordException;
 import com.sparta.spartascheduler.exception.UserAlreadyExistsException;
 import com.sparta.spartascheduler.util.JwtUtil;
 import com.sparta.spartascheduler.repository.UserRepository;
@@ -25,12 +27,19 @@ public class UserService {
     @Autowired
     private JwtUtil jwtUtil;
 
-    // Optional 을 사용하여 중복된 이메일 체크
     public User register(User user) {
         // 중복된 이메일 체크 및 예외 발생
         userRepository.findByEmail(user.getEmail())
-                .orElseThrow(() -> new UserAlreadyExistsException("중복된 이메일입니다."));
+                .ifPresent(existingUser -> {
+                    throw new UserAlreadyExistsException("중복된 이메일입니다.");
+                });
 
+        // 유효성 검사
+        try {
+            user.validate(); // validate() 메서드를 호출하여 이메일과 비밀번호를 검증
+        } catch (InvalidEmailException | InvalidPasswordException e) {
+            throw new RuntimeException(e.getMessage()); // RuntimeException 으로 감싸서 처리
+        }
 
         // 비밀번호 암호화 및 역할 설정
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -40,7 +49,6 @@ public class UserService {
         return userRepository.save(user);
     }
 
-
     public String generateToken(String username, String role) {
         return jwtUtil.generateToken(username, role); // 역할 추가
     }
@@ -48,5 +56,4 @@ public class UserService {
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
-
 }
